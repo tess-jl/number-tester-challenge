f36e392b5905c105f447e96b08e2f1a36c98a961
Tess Lameyer <tjlameyer@gmail.com>
2019-07-11 20:08:08 -0700

    first commit

---
+++ test/tests.js
@@ -0,0 +1,13 @@
+const test = QUnit.test;
+
+test('time to test!', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+
+    //Assert
+    // What should the result be?
+    assert.equal(true, false);
+});
